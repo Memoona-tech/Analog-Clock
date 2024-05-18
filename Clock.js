cf5664f7d5a589a5fbce5f0c3ad777a2e6df128d
Memoona Saleem <156640976+Memoona-tech@users.noreply.github.com>
2024-05-19 01:02:12 +0500

    Add files via upload

---
+++ Clock.js
@@ -0,0 +1,23 @@
+let hr = document.getElementById('hour'); 
+let mint = document.getElementById('mint');
+let sec = document.getElementById('sec');
+
+function displayTime(){
+    let date = new Date();
+
+    // Getting only hours , mints , sec from date
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    mint.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+}
+
+ setInterval(displayTime , 1000);
